d49093caae8a7dd3bf419bf99c404f24bcd9a8ad
yashaswi67 <yashubtech22@gmail.com>
2022-07-06 16:13:18 +0530

    firstcommit

---
+++ src/main/java/com/example/com/helloworld/Application.java
@@ -0,0 +1,13 @@
+package com.example.com.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
